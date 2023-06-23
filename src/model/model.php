e8a0c9faa2b8fb07ec11942f3a65bf70a5464111
yagoFogaca <yagofogaca20@gmail.com>
2023-06-23 19:18:21 -0300

    feat: criação do metodo de criar um chamado

--- src/model/model.php
+++ src/model/model.php
@@ -20,11 +20,24 @@ class Model
     $query->bindValue(':email', $user['email']);
     $query->execute();
     $getUser = $query->fetch(\PDO::FETCH_OBJ);
-
     if (!$getUser || $getUser->senha !== $user['password']) {
       throw new \PDOException('Email ou senha invalidos');
     }
 
     return $getUser;
+  }
+
+  public function createChamado($chamado)
+  {
+    $query = $this->db->prepare('INSERT INTO chamados (titulo, categoria, descricao VALUES (:titulo, :categoria, :descricao)');
+    $query->bindValue(":titulo", $chamado['titulo']);
+    $query->bindValue(":categoria", $chamado['categoria']);
+    $query->bindValue(":descricao", $chamado['descricao']);
+    $results = $query->execute();
+    if (!$results) {
+      throw new \PDOException('Erro ao criar o chamado');
+    }
+
+    return true;
   }
 }
